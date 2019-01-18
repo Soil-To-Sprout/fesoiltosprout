4d0943a207fbf65beff0ab781c2cb1f99fa2dd09
Kevin Harrison <kharrison7@elon.edu>
2019-01-17 21:37:58 -0500

    Updates

---
+++ precache-manifest.cde5c82241c7eb0f53bdf6bf156623a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fesoiltosprout/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1034ac76e7d1a3a57e80",
+    "url": "/fesoiltosprout/static/js/runtime~main.1034ac76.js"
+  },
+  {
+    "revision": "8f21f63f0ff56ddb444b",
+    "url": "/fesoiltosprout/static/js/main.8f21f63f.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/fesoiltosprout/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "8f21f63f0ff56ddb444b",
+    "url": "/fesoiltosprout/static/css/main.c8875ec2.chunk.css"
+  },
+  {
+    "revision": "5c4dfa6af76a276b95da690a2c849ce2",
+    "url": "/fesoiltosprout/index.html"
+  }
+];
